dde0470c6a665454c10a67f54fffd9226291304e
lemonliu1993 <2305247091@qq.com>
2024-01-09 23:15:04 +0800

    mavendemo: 单一架构: 查询单个对象

--- maven/proj05/src/main/java/com/lemon/imperial/court/dao/BaseDao.java
+++ maven/proj05/src/main/java/com/lemon/imperial/court/dao/BaseDao.java
@@ -1,6 +1,11 @@
 package com.lemon.imperial.court.dao;
 
+import com.lemon.imperial.court.util.JDBCUtils;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * Created by lemoon on 2024/1/8 23:53
@@ -11,9 +16,20 @@ public class BaseDao<T> {
 
     /**
      * 查询单个对象
+     *
      * @param sql
      * @param enntityClass
-     * @return
+     * @param parameters   传给 SQL 语句的参数
+     * @return 查询到的实体类对象
      */
-    public T getSingleBean(String sql, Class<T> enntityClass,Object ...){}
+    public T getSingleBean(String sql, Class<T> enntityClass, Object... parameters) {
+        try {
+            Connection connection = JDBCUtils.getConnection();
+
+            return runner.query(connection, sql, new BeanHandler<>(enntityClass), parameters);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
 }
