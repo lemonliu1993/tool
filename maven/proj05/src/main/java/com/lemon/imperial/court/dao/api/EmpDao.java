65922a17316acac782c2747baaa0f909086ebf1f
lemonliu1993 <2305247091@qq.com>
2024-01-12 23:05:32 +0800

    mavendemo: 单一架构: 子类和实现类

---
+++ maven/proj05/src/main/java/com/lemon/imperial/court/dao/api/EmpDao.java
@@ -0,0 +1,7 @@
+package com.lemon.imperial.court.dao.api;
+
+/**
+ * Created by lemoon on 2024/1/12 23:02
+ */
+public interface EmpDao {
+}
---
+++ maven/proj05/src/main/java/com/lemon/imperial/court/dao/impl/MemorialsDaoImpl.java
@@ -0,0 +1,7 @@
+package com.lemon.imperial.court.dao.impl;
+
+/**
+ * Created by lemoon on 2024/1/12 23:02
+ */
+public class MemorialsDaoImpl {
+}
