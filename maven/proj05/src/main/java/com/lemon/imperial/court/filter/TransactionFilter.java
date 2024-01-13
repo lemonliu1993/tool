1b2d7d44dad73f60b206123e2eebf47c348ee3b3
lemonliu1993 <2305247091@qq.com>
2024-01-13 23:52:08 +0800

    mavendemo: 单一架构: 配置filter

---
+++ maven/proj05/src/main/java/com/lemon/imperial/court/filter/TransactionFilter.java
@@ -0,0 +1,24 @@
+package com.lemon.imperial.court.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by lemoon on 2024/1/13 23:51
+ */
+public class TransactionFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
